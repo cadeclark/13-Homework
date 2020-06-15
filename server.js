4bb72817421b799da1517f3a68940e36c486b586
unknown <crazycademan@gmail.com>
2020-06-15 11:26:45 -0600

    m

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var burgerController = require("./controllers/burgers_controller.js");
+var burger = require("./models/burger.js");
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+app.use("/api/burgers", burgerController);
+
+app.listen(PORT);
