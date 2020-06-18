a6f0d15a2b116af57cff52e01018ec5b535642ab
Cade Clark <crazycademan@gmail.com>
2020-06-17 18:25:44 -0600

    Deleted some lines

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var burgerController = require("./controllers/burgers_controller.js");
 var burger = require("./models/burger.js");
 var PORT = process.env.PORT || 8080;
@@ -7,7 +6,6 @@ var app = express();
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
 var exphbs = require("express-handlebars");
 
